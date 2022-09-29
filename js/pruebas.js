2b246d1b785c92df51a550e190acd4f4c2eaabc9
Julian Vargas <julianvargastrb@gmail.com>
2022-09-28 22:10:41 -0500

    inspiraciones y pruebas js

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,1 +1,21 @@
 // Aqui pondras en practica posibles actualizaciones y programas de programacion Competitiva
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+/*Use like so*/
+
+async function timeSensativeAction(){ //must be async func
+  //do something here
+  await sleep(5000) //wait 5 seconds
+  //continue on...
+}
+
+function suma(){
+    var a= 0
+
+    while (a != 100){
+        a += 1
+       console.log(a)
+    }
+}
---
+++ js/webpersonal.js
@@ -0,0 +1,48 @@
+// alert("js funciona")
+
+function retornarLienzo(x) {
+    var canvas = document.getElementById(x);
+    if (canvas.getContext) {
+        var lienzo = canvas.getContext("2d");   
+        return lienzo;
+    } else
+        return false;
+}
+
+function dibujar() {
+    var lienzo=retornarLienzo("lienzo1");
+    if (lienzo) {
+        lienzo.fillStyle="rgb(255,0,0)";
+        lienzo.fillRect(10,10,50,50);
+        lienzo.fillStyle="rgba(0,255,0,0.3)";
+        lienzo.fillRect(40,40,50,50);
+    }
+}
+
+function sumaRepetida(){
+    let input_first  = Number(prompt());
+    let input_second = Number(prompt());
+
+    var a= 0
+
+    while (input_first != 100){
+        input_first += 1
+        a += input_first
+       console.log(a)
+    }
+    alert(a)
+}
+
+function saludo(edad, genero){
+    if (edad > 18 ){
+        if (genero = "M"){
+            alert("Buen dia señor")
+        }else{
+            alert("Buen dia señora")
+        }
+    }else if (edad = 18){
+        alert("°~°")
+    }else{
+        alert("D:<")
+    }
+}
